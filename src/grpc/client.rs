a1ed5536bcd065ea9eced5173f7ed0428e011ddd
Nick Platt <platt.nicholas@gmail.com>
2019-02-10 23:57:36 -0500

    tidier client impl

--- src/grpc/client.rs
+++ src/grpc/client.rs
@@ -1,4 +1,7 @@
 use {
+    http::Uri,
+    log::error,
+    std::net::SocketAddr,
     tokio::{
         executor::DefaultExecutor,
         net::TcpStream,
@@ -8,7 +11,6 @@ use {
     tower_h2::client::Connection,
     tower_http::AddOrigin,
 };
-
 use chord_rpc::v1::client::Chord;
 
 pub struct Client {
@@ -16,22 +18,22 @@ pub struct Client {
 }
 
 impl Client {
-    pub fn new() -> Self {
-        let addr = "[::1]:32031".parse().unwrap();
-        let uri: http::Uri = "http://localhost:32031".parse().unwrap();
-
-        let client = TcpStream::connect(&addr)
+    pub fn new(addr: &SocketAddr, origin: Uri) -> Self {
+        let client = TcpStream::connect(addr)
+            .map_err(|err| error!("tcp connect failed; err={:?}", err))
             .and_then(move |sock| {
                 Connection::handshake(sock, DefaultExecutor::current())
-                    .map_err(|err| panic!("http/2 handshake failed; err={:?}", err))
+                    .map_err(|err| error!("http/2 handshake failed; err={:?}", err))
             })
             .map(move |conn| {
                 use tower_http::add_origin::Builder;
-                let conn = Builder::new().uri(uri).build(conn).unwrap();
+                let conn = Builder::new()
+                    .uri(origin)
+                    .build(conn)
+                    .unwrap();
 
                 Chord::new(conn)
             })
-            .map_err(|err| eprintln!("connect failed; err={:?}", err))
             .wait()
             .unwrap();
 
