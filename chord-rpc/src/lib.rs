c21cf8d34dd0fa55fa1e1008d678acfc7611623d
Nick Platt <platt.nicholas@gmail.com>
2019-03-19 10:03:50 +0100

    bump prost and chord.proto

--- chord-rpc/src/lib.rs
+++ chord-rpc/src/lib.rs
@@ -5,7 +5,6 @@ pub use self::chord::v1;
 #[allow(dead_code)]
 mod chord {
     pub mod v1 {
-        use prost_derive::{Enumeration, Message};
         include!(concat!(env!("OUT_DIR"), "/chord.v1.rs"));
     }
 }
